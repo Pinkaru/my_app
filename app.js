583fd3d52376c3d9a61d019794640b3518fa03ee
pinkaru <jhjclever@gmail.com>
2016-05-09 00:38:34 +0900

    update 3

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var express = require('express');
 var path = require('path');
 var app=express();
 
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
+var data={count:0};
 
-console.log(__dirname);
 
-
+app.get('/',function(req,res){
+  data.count++;
+  console.log(req); console.log(res);
+  res.render('my_first_ejs',data);
+});
+app.get('/reset',function(req,res){
+  data.count=0;
+  console.log(req); console.log(res);
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count',function(req,res){
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+  console.log(req); console.log(res);
+});
+app.get('/set/:num',function(req,res){
+  data.count=req.params.num;
+  console.log(req); console.log(res);
+  res.render('my_first_ejs',data);
+});
 app.listen(3000,function(){
   console.log('server On!');
 });
