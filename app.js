73d819becbac17ea5eff7ce4fcb4b31f536797e2
pinkaru <jhjclever@gmail.com>
2016-05-09 01:50:37 +0900

    update 4

--- app.js
+++ app.js
@@ -1,30 +1,61 @@
 var express = require('express');
 var path = require('path');
 var app=express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://test:test@ds017582.mlab.com:17582/pinkaru_data");
+var db = mongoose.connection;
+db.once("open",function(){
+  console.log("DB connected!");
+});
+db.on("error",function(err){
+  console.log("DB ERROR :",err);
+});
+
+var dataSchema = mongoose.Schema({
+  name : String,
+  count : Number
+});
+var Data=mongoose.model('data',dataSchema);
+Data.findOne({name:"myData"},function(err,data){
+  if(err) return console.log("Data ERROR:",err);
+  if(!data){
+    Data.create({name:"myData",count:0},function(err,data){
+      if(err) return console.log("Data ERROR:",err);
+      console.log("Counter initialized :",data);
+    });
+  }
+});
 
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
-var data={count:0};
+
 
 
 app.get('/',function(req,res){
-  data.count++;
-  console.log(req); console.log(res);
-  res.render('my_first_ejs',data);
+  Data.findOne({name:"myData"},function(err,data){
+    if(err) return console.log("Data ERROR:",err);
+    data.count++;
+    data.save(function(err){
+      if(err)return console.log("Data ERROR:",err);
+      res.render('my_first_ejs',data);
+    });
+  });
 });
+
 app.get('/reset',function(req,res){
   data.count=0;
-  console.log(req); console.log(res);
+
   res.render('my_first_ejs',data);
 });
 app.get('/set/count',function(req,res){
   if(req.query.count) data.count=req.query.count;
   res.render('my_first_ejs',data);
-  console.log(req); console.log(res);
+
 });
 app.get('/set/:num',function(req,res){
   data.count=req.params.num;
-  console.log(req); console.log(res);
+
   res.render('my_first_ejs',data);
 });
 app.listen(3000,function(){
